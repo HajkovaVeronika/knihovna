71c53ad39516e03e0c28c54599f37071bfcd65a1
Veronika Hájková <HajkovaVeronika@litv.sssvt.cz>
2026-01-05 09:05:06 +0100

    feat: created project by template

---
+++ src/knihovnaWebApplication/knihovnaWebApplication.WebMvcApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace knihovnaWebApplication.WebMvcApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
