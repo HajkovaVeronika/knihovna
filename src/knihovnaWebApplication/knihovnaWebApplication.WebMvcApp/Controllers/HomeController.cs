7d897478dec7651051e24f0282037456495d54a9
Veronika Hájková <HajkovaVeronika@sssvt.cz>
2026-01-18 20:54:59 +0100

    changed home page and added style

--- src/knihovnaWebApplication/knihovnaWebApplication.WebMvcApp/Controllers/HomeController.cs
+++ src/knihovnaWebApplication/knihovnaWebApplication.WebMvcApp/Controllers/HomeController.cs
@@ -1,22 +1,33 @@
-using System.Diagnostics;
+using knihovnaWebApplication.WebMvcApp.Data;
+using knihovnaWebApplication.WebMvcApp.Entities;
 using knihovnaWebApplication.WebMvcApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace knihovnaWebApplication.WebMvcApp.Controllers
 {
     public class HomeController : Controller
     {
+        public LibrariesDbContext DbContext { get; set; }
+        public List<Library> Libraries { get; set; }
+        public List<Book> Books { get; set; }
+        public List<User> Users { get; set; }
+       
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             //testuju commit
             _logger = logger;
+            DbContext = new LibrariesDbContext();
+            Libraries = DbContext.Libraries.ToList();
+            Books = DbContext.Books.ToList();
+            Users = DbContext.Users.ToList();
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(Libraries);
         }
 
         public IActionResult Privacy()
